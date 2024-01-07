25675c67cb76a7278f8daae610e95caacab65448
bovage <fayemibolu2@gmail.com>
2024-01-07 23:13:25 +0100

    first commit google oauth 2.0 implementation

---
+++ app.js
@@ -0,0 +1,108 @@
+import * as dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import fetch from "node-fetch";
+
+const app = express();
+app.use(express.json());
+
+const GOOGLE_OAUTH_URL = process.env.GOOGLE_OAUTH_URL;
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CALLBACK_URL = "http%3A//localhost:8000/google/callback";
+const GOOGLE_OAUTH_SCOPES = [
+  "https%3A//www.googleapis.com/auth/userinfo.email",
+  "https%3A//www.googleapis.com/auth/userinfo.profile",
+];
+
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+const GOOGLE_ACCESS_TOKEN_URL = process.env.GOOGLE_ACCESS_TOKEN_URL;
+
+const mongoDBURI = process.env.MONGO_DB_URI;
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+
+mongoose.connect(mongoDBURI);
+
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    unique: true,
+    trim: true,
+    required: [true, "Please provide a  name"],
+    minlength: 3,
+    maxlength: 56,
+  },
+  email: {
+    type: String,
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please provide a valid email.",
+    ],
+    unique: true,
+  },
+  password: {
+    type: String,
+    minlength: 6,
+    required: [true, "Provide a password"],
+  },
+});
+
+UserSchema.methods.generateToken = function () {
+  const token = jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_LIFETIME,
+  });
+  return token;
+};
+
+const User = mongoose.model("User", UserSchema);
+
+app.get("/", async (req, res) => {
+  const state = "some_state";
+  const scopes = GOOGLE_OAUTH_SCOPES.join(" ");
+  const GOOGLE_OAUTH_CONSENT_SCREEN_URL = `${GOOGLE_OAUTH_URL}?client_id=${GOOGLE_CLIENT_ID}&redirect_uri=${GOOGLE_CALLBACK_URL}&access_type=offline&response_type=code&state=${state}&scope=${scopes}`;
+  res.redirect(GOOGLE_OAUTH_CONSENT_SCREEN_URL);
+});
+
+app.get("/google/callback", async (req, res) => {
+  console.log(req.query);
+  const { code } = req.query;
+  const data = {
+    code,
+    client_id: GOOGLE_CLIENT_ID,
+    client_secret: GOOGLE_CLIENT_SECRET,
+    redirect_uri: "http://localhost:8000/google/callback",
+    grant_type: "authorization_code",
+  };
+  console.log(data);
+  // exchange authorization code for access token
+  const response = await fetch(GOOGLE_ACCESS_TOKEN_URL, {
+    method: "POST",
+    body: JSON.stringify(data),
+  });
+  const access_token_data = await response.json();
+  const { id_token } = access_token_data;
+  console.log(id_token);
+  // verify and extract the information in the id token
+  const token_info_response = await fetch(
+    `${process.env.GOOGLE_TOKEN_INFO_URL}?id_token=${id_token}`
+  );
+  const token_info_data = await token_info_response.json();
+
+  const { email, name, at_hash: password } = token_info_data;
+  let user = await User.findOne({ email }).select("-password");
+  if (!user) {
+    user = await User.create({ email, name, password }).select("-password");
+  }
+  const token = user.generateToken();
+  res.status(token_info_response.status).json({ user, token });
+});
+
+const PORT = process.env.PORT || 3000;
+
+const start = async (port) => {
+  app.listen(port, () => {
+    console.log(`Server running on port: http://localhost:${port}`);
+  });
+};
+
+start(PORT);
